19333ceb32c9190d5a4c97fdf928176ce9e9fa41
Atif Hussain <atifhussain28@gmail.com>
2024-05-20 02:46:28 +0100

    stash commit

--- backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+840aba2daad2320bf47002d776807c978106a466")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3966807e796b753365f719554ef6f0eee47aad17")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
