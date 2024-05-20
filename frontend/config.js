a0277c4a22868f5ba31f4f76a2539f612a93bc4e
Atif Hussain <atifhussain28@gmail.com>
2024-05-20 02:44:52 +0100

    Added config.js

---
+++ frontend/config.js
@@ -0,0 +1,12 @@
+const config = {
+    TEMPERATURE_LIMIT : 3,
+    RAINFALL_LIMIT : 2,
+    HUMIDITY_LIMIT : 1,
+    AIR_POLLUTION_LIMIT : 1,
+    CO2_EMISSIONS_LIMIT : 5,
+
+    TEMPERATURE_MIN_THRESHOLD : -9,
+    TEMPERATURE_MAX_THRESHOLD : 30,
+};
+
+export default config;
